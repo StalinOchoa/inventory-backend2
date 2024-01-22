a0b3e0357732d22c165a482f3a13287ba44f2389
StalinOchoa <stalin.or7@live.com>
2024-01-21 19:16:49 -0500

    buscar categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,10 @@ public class CategoryRestController {
 	
 	@Autowired
 	private ICategoryService service;
-	
+	/**
+	 * get all categories OBTENIENDO CATEGORIAS
+	 * @return
+	 */
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories() {
 		
@@ -25,6 +29,18 @@ public class CategoryRestController {
 		
 	}
 	
+	/**
+	 * get categories by id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
+		
+		 ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		 return response;
+		
+	}
 	
-
+	
 }
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,6 +36,40 @@ public class CategoryServiceImpl implements ICategoryService{
 		} catch (Exception e) {
 			
 			response.setMetadata("Respuesta Nok", "-1", "Error al consultar");
+			//para imprimir en logs
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+			
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+    //metodo buscar por ID
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			//este objeto devuelve un objeto optional busca en la bdd
+			Optional<Category> category = categoryDao.findById(id);
+			//si la categoria existe por id
+			if (category.isPresent()) {
+				//se agrega la categoria a la lista
+					list.add(category.get());
+					response.getCategoryRest().setCategory(list);
+					response.setMetadata("Respuesta ok", "00", "Categoria  econtrada");
+			} else {
+				response.setMetadata("Respuesta Nok", "-1", "Categoria no econtrada");
+				//para imprimir en logs
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			
+			response.setMetadata("Respuesta Nok", "-1", "Error al consultar por id");
 			//para imprimir en logs
 			e.getStackTrace();
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
